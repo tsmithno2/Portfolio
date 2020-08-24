518b0d3a6aab85083292bf44371287b5d8baedb4
Trevor Smith <t.smithno2@gmail.com>
2020-08-24 11:49:41 -0600

    Basics again

---
+++ components/intro/Intro.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import "./Intro.css";
+
+function Intro() {}
+
+return (
+  <div>
+    <p>Intro</p>
+  </div>
+);
---
+++ components/skills/Skills.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import "./Skills.css";
+
+function Header() {}
+
+return (
+  <div>
+    <p>Skills</p>
+  </div>
+);
