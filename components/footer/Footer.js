520acb0a6ca2931629ef15aee072ca30c6b6053e
Trevor Smith <t.smithno2@gmail.com>
2020-08-24 11:41:45 -0600

    Changed some typos in the names

---
+++ components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import "./footer.css";
+
+function Footer() {}
+
+return (
+  <div>
+    <p>footer</p>
+  </div>
+);
---
+++ components/header/Header.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import "./experience.css";
+
+function Header() {}
+
+return (
+  <div>
+    <p>Header</p>
+  </div>
+);
