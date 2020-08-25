f8c081bbce1a2af75f9320d40a1ec85095a06e33
Trevor Smith <t.smithno2@gmail.com>
2020-08-25 11:23:12 -0600

    added basic buttons for header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,14 @@ import "./Header.css";
 
 function Header() {
   return (
-    <div>
+    <div className="Header">
+      <div className="Buttons">
+        <button>Home</button>
+        <button>Skills</button>
+        <button>Experience</button>
+        <button>About Me</button>
+        <button>Contact</button>
+      </div>
       <p>Header</p>
     </div>
   );
