a0363c9b9ae2d83f35feb33ab6ba037edd0a8374
Trevor Smith <t.smithno2@gmail.com>
2020-08-31 14:30:32 -0600

    got a basic css started up

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import Experience from "./components/experience/Experience.js";
 import Footer from "./components/footer/Footer.js";
 import Header from "./components/header/Header.js";
-import Intro from "./components/intro/Intro.js";
 import Skills from "./components/skills/Skills.js";
+import Intro from "./components/intro/Intro";
 
 function App() {
   return (
--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,12 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import "./Intro.css";
+import { render } from "@testing-library/react";
 
-function Intro() {
-  return (
-    <div>
-      <p>Intro</p>
-    </div>
-  );
+class Intro extends Component {
+  checkButtonClick() {
+    console.log("Big boy works");
+  }
+
+  check2ndButtonClick() {
+    console.log("Little Fella Works works");
+  }
+
+  render() {
+    return (
+      <div className="Intro">
+        <div className="TopIntro">
+          <p>Intro Top 2/3</p>
+          <button
+            onClick={() => {
+              this.checkButtonClick();
+            }}
+          >
+            Press me to check
+          </button>
+        </div>
+        <div className="BottomIntro">
+          <p>Intro Top 2/3</p>
+          <button
+            onClick={() => {
+              this.check2ndButtonClick();
+            }}
+          >
+            Press me to check
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Intro;
